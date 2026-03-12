53d390204f8a6a208a8daf7d842052ec7c0338a9
LamThanh2k04 <ngolam2k04@gmail.com>
2026-03-12 20:45:08 +0700

    update

--- src/routers/admin/accountRouter.js
+++ src/routers/admin/accountRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { accountController } from "../../controllers/admin/accountController.js";
+import { upload } from "../../common/cloudinary/initCloudinary.js";
 
 const router = express.Router()
 
-router.post('/createAccount',accountController.createAccount)
-router.put('/updateInfoAccount/:accountId',accountController.updateInfoAccount)
+router.post('/createAccount',upload.single('avatar'),accountController.createAccount)
+router.put('/updateInfoAccount/:accountId',upload.single('avatar'),accountController.updateInfoAccount)
 router.put('/updateAccountStatus/:accountId',accountController.updateAccountStatus)
 router.get('/getAllAcounts',accountController.getAllAcounts)
 router.get('/getAllPatients',accountController.getAllPatients)
